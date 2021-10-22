439dacf45997e20265fc21f75e1906006ff45bb5
syndefi <syndefi@protonmail.com>
2021-10-22 15:19:19 +0800

    feat: 2.shadow,str2u32

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::io;
-
+use rand::Rng;
+use std::cmp::Ordering; //trait
 fn main() {
     println!("猜数!");
+    let secret_number = rand::thread_rng().gen_range(1..100);
+    println!("神秘数字是 {}", secret_number);
 
     println!("猜测一个数!");
 
     let mut guess = String::new();
-
     io::stdin().read_line(&mut guess).expect("无法读取行");
+    //shadow
+    let guess = guess.trim().parse::<u32>().expect("please type a number");
 
+    match guess.cmp(&secret_number) {
+        Ordering::Less =>  println!("too small"), //arm
+        Ordering::Greater =>  println!("too big"),
+        Ordering::Equal =>  println!("Equal"),
+    }
     println!("你猜的数是：{}", guess);
 }
