2b6bc1db79562742cbb3e28abd65434b32ccde55
syndefi <syndefi@protonmail.com>
2021-10-22 15:26:40 +0800

    feat: 3.match处理错误,循环

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..100);
     println!("神秘数字是 {}", secret_number);
 
-    println!("猜测一个数!");
+    loop {
+        println!("-------------\n猜测一个数!");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("无法读取行");
-    //shadow
-    let guess: u32 = guess.trim().parse::<u32>().expect("please type a number");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small"), //arm
-        Ordering::Greater => println!("too big"),
-        Ordering::Equal => println!("Equal"),
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+        //shadow
+        let guess: u32 = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"), //arm
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("Equal");
+                break;
+            }
+        }
+        println!("你猜的数是：{}", guess);
     }
-    println!("你猜的数是：{}", guess);
 }
