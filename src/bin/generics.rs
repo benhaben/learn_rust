87e950fc87e8f0eb1df02af88c7553ad68b4452f
syndefi <syndefi@protonmail.com>
2021-10-27 23:25:24 +0800

    feat:31.泛型

---
+++ src/bin/generics.rs
@@ -0,0 +1,28 @@
+struct Point<T,U>{
+    x:T,
+    y:U,
+}
+
+enum Option<T>{
+    Some(T),
+    None,
+}
+
+impl <T,U> Point<T,U> {
+    
+}
+
+impl Point<i32,u32>{
+
+}
+
+impl <T,U> Point<T,U> {
+    fn mixup<V,W>(self,other:Point<V,W>)->Point<T,W>{
+        Point{
+            x: self.x,
+            y:other.y,
+        }
+    }
+    
+}
+fn main(){}
