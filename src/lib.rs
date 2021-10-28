6161f5c877f9b131602a65c6e79d0131e0b239a9
syndefi <syndefi@protonmail.com>
2021-10-28 10:52:15 +0800

    feat:32.trait, template trait bound

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-mod Father;
+use std::fmt::{Debug, Display};
 
+mod Father;
 
 pub fn out() {
     crate::Father::Son::say(); //绝对路径。大部分人使用这个
     Father::Son::say(); //相对路径
 }
-
 
 // 模块不仅可以组织·代码，还可以定义私有边界
 // 如果想把函数或者struct等设为私有，可以将它放到某个模块中
@@ -18,7 +18,6 @@ pub fn out() {
 // use std::io::Result as ioResult;
 // 使用use导入的路径是默认私有的。可以使用pub use来导出
 // pub use std::io::Result as ioResult;
-
 
 // 使用外部包
 // 1、cargo.toml添加依赖包， https://crates.io/
@@ -49,3 +48,50 @@ pub fn out() {
 //     - Rust会从与模块同名的文件中加载内容
 //     - 模块树的结构不会变化
 // 随着模块变大，该技术可以让你把模块的内容移动到其他文件
+
+pub trait Summary {
+    fn summarize(&self) -> String {
+        /// 默认实现
+        println!("meiyoua");
+        "xxxx".to_string()
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+}
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}", self.headline)
+    }
+}
+
+//使用trait约束
+pub fn notify(item: impl Summary + Display) {
+    println!("news:{}", item.summarize());
+}
+
+// 这个更好
+pub fn notify1<T: Summary + Display>(item: T) {
+    println!("news:{}", item.summarize());
+}
+
+// 语法where, 约束太长怎么办
+pub fn notify2<T: Summary + Display, U: Clone + Debug>(item: T, b: U) {
+    println!("news:{}", item.summarize());
+}
+
+pub fn notify3<T, U>(item: T, b: U)
+where
+    T: Summary + Display,
+    U: Clone + Debug,
+{
+    println!("news:{}", item.summarize());
+}
+
+// trait实现返回类型.但必须只能返回同一种类型
+pub fn notifyR() -> impl Summary{
+    NewsArticle{
+        headline:"xxxx".to_string()
+    }
+}
