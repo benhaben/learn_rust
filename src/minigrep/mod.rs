7ce983e191cc097ef5d61ce4a023ff893edc508c
yin <shenyin@rocketmail.com>
2021-11-03 11:44:57 +0800

    feat:36.4.example minigrep unit test

--- src/minigrep/mod.rs
+++ src/minigrep/mod.rs
@@ -1,12 +1,14 @@
-use std::{env, error::Error, fs,  process};
+use std::{env, error::Error, fs, process};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("search for {}", config.query);
     println!("in file {}", config.filename);
     let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     println!("with test:\n{}", contents);
     Ok(())
-
 }
 
 pub struct Config {
@@ -15,15 +17,14 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config,&'static str> {
-
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments!");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
-         Ok(Config { query, filename })
+        Ok(Config { query, filename })
     }
 }
 
@@ -31,4 +32,30 @@ pub fn parse(args: &[String]) -> Config {
     let query = args[1].clone();
     let filename = args[2].clone();
     Config { query, filename }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        rust:
+        safe,faster,productive.
+        ";
+
+        assert_eq!(vec!["safe"], search(query, contents))
+    }
 }
