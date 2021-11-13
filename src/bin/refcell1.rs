62751a3c9f82f96e2a8bee32a397843b19cb5e69
yin <shenyin@rocketmail.com>
2021-11-13 20:38:23 +0800

    feat:44. Rc<T>,RefCell<T>

---
+++ src/bin/refcell1.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
+
+// 其他实现内部可变性的类型
+// Cell<T>: 通过复制来访问数据
+// Mutex<T>: 用于实现跨线程情形下的内部可变性模式
